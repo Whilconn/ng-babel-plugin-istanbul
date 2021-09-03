63383017adaf93a18e0b0a2c277076dd2330852a
whilconn <liuweikang@xiao100.com>
2021-09-03 09:31:43 +0800

    feat: 接入插桩工具 babel-plugin-istanbul

---
+++ coverage.webpack.js
@@ -0,0 +1,19 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        enforce: 'post',
+        include: require('path').join(__dirname, 'src'),
+        exclude: [/\.(e2e|spec)\.ts$/, /node_modules/, /(ngfactory|ngstyle)\.js/],
+      },
+    ],
+  }
+};
+
